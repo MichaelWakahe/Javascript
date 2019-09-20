8beddcb3f44cb6305d67dd67c6704630a7441cc4
Michael Wakahe <mikewakahe@gmail.com>
2019-09-20 11:34:39 +0300

    Ch 1 of Head First Javascript.

---
+++ Head First Javascript/Chapter 1/code.js
@@ -0,0 +1,13 @@
+let scoops = 5;
+console.log("scoops are " + scoops);
+
+while (scoops > 0) {
+	document.write("Another scoop!<br>");
+	if (scoops < 3) {
+		alert("Ice cream is running low!");
+	} else if (scoops >= 5) {
+		alert("Eat faster, the ice cream is going to melt!");
+	}
+	scoops = scoops - 1;
+}
+document.write("Life without ice cream isn't the same");
