b60699948ae2f0855ba230eb0a441ab995d1ff5e
Michael Wakahe <mikewakahe@gmail.com>
2022-05-03 17:05:43 +0300

    Rearrange of Essential Typescript 4 book folders

--- Books/Essential Typescript 4/Chapter 1/todo/src/todoItem.ts
+++ Books/Essential Typescript 4/Chapter 1/todo/src/todoItem.ts
@@ -9,6 +9,16 @@ export class TodoItem {
         this.complete = complete;
     }
     
+    // Below is a more compact way of writing the constructor above
+    // constructor(public id: number,
+    //     public task: string,
+    //     public complete: boolean = false) {
+    //     // no statements required
+    // }
+
+    // The `public` keyword is optional below; Typescript assumes all methods and properties are `public` unless another
+    // access level is used.
+    // The `public` keyword should however be used in constructors.
     public printDetails() : void {
         console.log(`${this.id}\t${this.task} ${this.complete
             ? "\t(complete)" : ""}`);
