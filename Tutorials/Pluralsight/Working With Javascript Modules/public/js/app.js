9536aa033683073cbc1d366b02b7bc4c83a355af
Michael Wakahe <mikewakahe@gmail.com>
2022-06-09 15:16:24 +0300

    Add sessionRepository.js - Pluralsight Js Modules

--- Tutorials/Pluralsight/Working With Javascript Modules/public/js/app.js
+++ Tutorials/Pluralsight/Working With Javascript Modules/public/js/app.js
@@ -1,15 +1,3 @@
-function getSessions() {
-    return new Promise(function (resolve, reject) {
-        var oReq = new XMLHttpRequest();
-        oReq.onload = function (e) {
-            resolve(e.target.response);
-        };
-        oReq.open('GET', 'sessions.json', true);
-        oReq.responseType = 'json';
-        oReq.send();
-    })
-}
-
 
 function render() {
     var list = document.querySelector('#sessions');
---
+++ Tutorials/Pluralsight/Working With Javascript Modules/public/js/sessionRepository.js
@@ -0,0 +1,21 @@
+const sessionURL = 'sessions.json';
+
+let sessionList = [];
+
+function getSessions() {
+    return new Promise(function (resolve, reject) {
+        var oReq = new XMLHttpRequest();
+        oReq.onload = function (e) {
+            sessionList = e.target.response
+            resolve(sessionList);
+        };
+        oReq.open('GET', sessionURL, true);
+        oReq.responseType = 'json';
+        oReq.send();
+    })
+}
+
+export { getSessions as default };
+// when 'default' is used, you can import the function into another module without needing to know the name.
+// For example: import anyName from './sessionRepository';
+// Here 'anyName' will have the value of 'getSessions'.
